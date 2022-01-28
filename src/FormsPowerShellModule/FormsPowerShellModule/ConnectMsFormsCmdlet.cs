b3696f213cd56489ace68f9c896005329b014d04
Pascal Ammon <pascal.ammon@flowsoft.ch>
2022-01-28 05:20:51 +0100

    add connect function

---
+++ src/FormsPowerShellModule/FormsPowerShellModule/ConnectMsFormsCmdlet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsPowerShellModule
+{
+    [Cmdlet(VerbsCommunications.Connect, "MsForms")]
+    public class ConnectMsFormsCmdlet : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string TenantId { get; set; }
+
+
+        [Parameter(Mandatory = true)]
+        public string ClientId { get; set; }
+
+
+        [Parameter(Mandatory = true)]
+        public PSCredential Credentials { get; set; }
+        
+        protected override void ProcessRecord()
+        {
+            FormsService.Connect(TenantId, ClientId, Credentials);
+            WriteVerbose("connected");
+        }
+    }
+}
---
+++ src/FormsPowerShellModule/FormsPowerShellModule/src/FormsService.cs
@@ -0,0 +1,32 @@
+﻿using System.Management.Automation;
+using System.Security;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace FormsPowerShellModule
+{
+    internal class FormsService
+    {
+        private static IPublicClientApplication _app;
+        private static PSCredential _credentials;
+        private static AuthenticationResult _result;
+
+        internal static void Connect(string tenantId, string clientId, PSCredential credentials)
+        {
+            _credentials = credentials;
+            _app = PublicClientApplicationBuilder.Create(clientId)
+                .WithAuthority($"https://login.microsoftonline.com/{tenantId}/oauth2/v2.0/token")
+                .Build();
+
+            AcquireToken().GetAwaiter().GetResult();
+        }
+
+        private static async Task AcquireToken()
+        {
+            _result = await _app.AcquireTokenByUsernamePassword(
+                    new[] {"api://forms.office.com/c9a559d2-7aab-4f13-a6ed-e7e9c52aec87/Forms.Read"},
+                    _credentials.UserName, _credentials.Password)
+                .ExecuteAsync();
+        }
+    }
+}
